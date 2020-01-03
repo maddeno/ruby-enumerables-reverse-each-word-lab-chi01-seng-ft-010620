714c22477caeaf37633b2a3c47b169329dc76618
Olivia Madden <madden.livi@gmail.com>
2020-01-03 23:00:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+#def reverse_each_word(string)
+ # array = string.split(" ")
+  #return_string = ""
+  #array.each{|word| return_string +="#{word.reverse} "}
+  #return_string[0..-2]
+#end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  return_string = array.collect{|word| "#{word.reverse} "}.join
+  return_string[0..-2]
+end
